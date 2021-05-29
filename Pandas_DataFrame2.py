85d8005308d3055d80ab31cf65222073d3cc88d7
nvarghese-u2can <84356761+nvarghese-u2can@users.noreply.github.com>
2021-05-28 22:44:44 -0400

    Add files via upload
    Pandas-DataFrame Examples

---
+++ Pandas_DataFrame2.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame(data=np.random.randn(5, 5), columns=['A', 'B', 'C', 'D', 'E'])
+print(df)
+print(df > 0)
+print('\n')
+print(df[df > 0])
+print('\n')
+print(df[(df > 0) & (df < 2)])
+print((df['A'] > 0))
+print('\n')
+print(df[df['A'] > 0]['E'])
+
+df1 = pd.DataFrame(data=np.random.randn(3, 3), index=['a', 'b', 'c'], columns=['A', 'B', 'C'])
+print(df1)
+print(df1.reset_index())
+print('\n')
+df1['states'] = ['AB', 'BC', 'CD']
+print(df1.set_index('states'))
+df2=df1.set_index('states', inplace=True)
+print(df2)
+
+
+
+
+
+
+
+
